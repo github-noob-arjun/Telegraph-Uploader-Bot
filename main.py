8d692eb1f68a9b73367c36c8111ccfe94c3ae2bb
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-12-15 12:36:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 Bot = Client(
     "Telegraph Uploader Bot",
     bot_token="5931770179:AAHoonbw-4iJF6_rCzj02sph_6wNTkUssFs",
-
     api_id="15681435",
-
     api_hash="29021e7d8f6fe5338a45470115567f9e"
 )
 
